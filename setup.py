ede8dae1e9aced51e09489cb8974a0664213377f
eaedk <emmanuelkoupoh@gmail.com>
2020-09-03 09:37:21 +0000

    getpass

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
-    install_requires=["tdqm", "requests", "pandas", "getpass"],
+    install_requires=["tdqm", "requests", "pandas", ], # "getpass"
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
