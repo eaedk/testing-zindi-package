51461dee7c607adf4bdf9c7adda0252a78d54382
eaedk <emmanuelkoupoh@gmail.com>
2020-11-18 15:48:21 +0000

    ...

--- tests/test.py
+++ tests/test.py
@@ -8,12 +8,12 @@ sys.path.append(parentdir)
 from zindi.user import Zindian
 
 ## FOR TEST
-USERNAME, PASSWORD = "####", "####"
+USERNAME, PASSWORD = "eaedk", "Eaedk.01021999" # "####", "####"
 user = Zindian(username=USERNAME, fixed_password=PASSWORD)
 
 user.which_challenge
 
-user.select_a_challenge(fixed_index=3) # 
+user.select_a_challenge(reward="knowledge" , fixed_index=2) # 
 
 user.which_challenge
 
@@ -25,6 +25,6 @@ user.my_rank
 
 # user.submit(filepaths=['tests/dataset/SampleSubmission.csv'])
 
-user.submission_board()
+# user.submission_board()
 
-user.create_team(team_name="New Team")
+# user.create_team(team_name="New Team")
