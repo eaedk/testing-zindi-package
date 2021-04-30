ac4cf4d0eb5c6fb52425c49e9a06628d3a91da3e
Tim-Abwao <abwaomusungu@gmail.com>
2021-04-30 11:34:15 +0300

    Add sphinx docs
    Create source files to generate the docs using sphinx.

--- docs/utils/print_challenges.py
+++
@@ -1,8 +0,0 @@
-def print_challenges(challenges_data):
-    """Formated print the Zindi's challenge as table.
-
-    Parameters
-    ----------
-    challenges_data : dictionary | json
-        The json's response of the request to get informations about the challenges.
-    """
